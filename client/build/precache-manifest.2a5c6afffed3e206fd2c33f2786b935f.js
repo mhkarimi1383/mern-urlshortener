346a13967b5d45042a32c531b8493475daea0b47
Ethan Vernon <ethan.vernon@colorado.edu>
2019-03-16 12:36:36 +0800

    includes client folder for tracking

---
+++ client/build/precache-manifest.2a5c6afffed3e206fd2c33f2786b935f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05b7a065f73c91f3bb0f",
+    "url": "/static/js/main.aefe1162.chunk.js"
+  },
+  {
+    "revision": "ac6c3f7920ddd542dd15",
+    "url": "/static/js/2.cf38e645.chunk.js"
+  },
+  {
+    "revision": "7843150e74fc5795e904e759f7cdbd1e",
+    "url": "/index.html"
+  }
+];

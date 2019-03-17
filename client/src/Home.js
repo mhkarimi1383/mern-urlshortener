4d0157e878f3b3109ec54398d6554b27bca07572
Ethan Vernon <ethan.vernon@colorado.edu>
2019-03-17 15:06:54 +0800

    moves all logic to Home.js from App.js. App.js contains only router

--- client/src/Home.js
+++ client/src/Home.js
@@ -12,9 +12,108 @@ import {Footer} from './Footer';
 
 export class Home extends Component {
 
+	// initialize our state 
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			data: [],
+			id: 0,
+			message: null,
+			intervalIsSet: false,
+			idToDelete: null,
+			idToUpdate: null,
+			objectToUpdate: null
+		};
+
+		/*this.getDataFromDb = this.getDataFromDb.bind(this);
+		this.putDataToDB = this.putDataToDB.bind(this);
+		this.deleteFromDB = this.deleteFromDB.bind(this);
+		this.updateDB = this.updateDB.bind(this);*/
+	}
+
+	// when component mounts, first thing it does is fetch all existing data in our db
+	// then we incorporate a polling logic so that we can easily see if our db has 
+	// changed and implement those changes into our UI
 	componentDidMount() {
 		window.scrollTo(0,0);
+
+		this.getDataFromDb();
+		if (!this.state.intervalIsSet) {
+			let interval = setInterval(this.getDataFromDb, 1000);
+			this.setState({ intervalIsSet: interval });
+		}
 	}
+
+	// never let a process live forever 
+	// always kill a process everytime we are done using it
+	componentWillUnmount() {
+		if (this.state.intervalIsSet) {
+			clearInterval(this.state.intervalIsSet);
+			this.setState({ intervalIsSet: null });
+		}
+	}
+
+	// our first get method that uses our backend api to 
+	// fetch data from our data base
+	getDataFromDb = () => {
+		fetch("/api/getData")
+			.then(data => data.json())
+			.then(res => this.setState({ data: res.data }))
+			.then(console.log(this.state.data));
+	};
+
+	// our put method that uses our backend api
+	// to create new query into our data base
+	putDataToDB = message => {
+		let currentIds = this.state.data.map(data => data.id);
+		let idToBeAdded = 0;
+		while (currentIds.includes(idToBeAdded)) {
+			++idToBeAdded;
+		}
+
+		axios.post("/api/putData", {
+			id: idToBeAdded,
+			message: message
+		});
+	};
+
+	// our delete method that uses our backend api 
+	// to remove existing database information
+	deleteFromDB = idTodelete => {
+		let objIdToDelete = null;
+		this.state.data.forEach(dat => {
+			if (dat.id == idTodelete) {
+				objIdToDelete = dat._id;
+			}
+		});
+
+		console.log(objIdToDelete);
+
+		axios.delete("/api/deleteData", {
+			data: {
+				id: objIdToDelete
+			}
+		});
+	};
+
+	// our update method that uses our backend api
+	// to overwrite existing data base information
+	updateDB = (idToUpdate, updateToApply) => {
+		let objIdToUpdate = null;
+		this.state.data.forEach(dat => {
+			if (dat.id == idToUpdate) {
+				objIdToUpdate = dat._id;
+			}
+		});
+
+		axios.post("/api/updateData", {
+			id: objIdToUpdate,
+			update: { message: updateToApply }
+		});
+	};
+
 
 	
 	render() {
@@ -189,6 +288,74 @@ export class Home extends Component {
 			<Footer/>
 
 		</div>
-	    );
-  }
+		);
+	}
 }
+
+
+/*
+	render() {
+
+	    const { data } = this.state;
+	    
+	    return (
+			<div>
+				<ul>
+					{data.length <= 0
+						? "NO DB ENTRIES YET"
+						: data.map(dat => (
+							<li style={{ padding: "10px" }} key={data.message}>
+								<span style={{ color: "gray" }}> id: </span> {dat.id} <br />
+								<span style={{ color: "gray" }}> data: </span>
+								{dat.message}
+							</li>
+						))}
+				</ul>
+				<div style={{ padding: "10px" }}>
+					<input
+						type="text"
+						onChange={e => this.setState({ message: e.target.value })}
+						placeholder="add something in the database"
+						style={{ width: "200px" }}
+					/>
+					<button onClick={() => this.putDataToDB(this.state.message)}>
+						ADD
+					</button>
+				</div>
+				<div style={{ padding: "10px" }}>
+					<input
+						type="text"
+						style={{ width: "200px" }}
+						onChange={e => this.setState({ idToDelete: e.target.value })}
+						placeholder="put id of item to delete here"
+					/>
+					<button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
+						DELETE
+					</button>
+				</div>
+				<div style={{ padding: "10px" }}>
+					<input
+						type="text"
+						style={{ width: "200px" }}
+						onChange={e => this.setState({ idToUpdate: e.target.value })}
+						placeholder="id of item to update here"
+					/>
+					<input
+						type="text"
+						style={{ width: "200px" }}
+						onChange={e => this.setState({ updateToApply: e.target.value })}
+						placeholder="put new value of the item here"
+					/>
+					<button
+						onClick={() =>
+							this.updateDB(this.state.idToUpdate, this.state.updateToApply)
+							}
+						>
+							UPDATE
+						</button>
+				</div>
+			</div>
+		);
+	}
+}
+*/

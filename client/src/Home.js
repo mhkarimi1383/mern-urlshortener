9ad51338f419445df1c98772746584b2c3a79b6f
Ethan Vernon <ethan.vernon@colorado.edu>
2019-03-21 22:18:19 +0800

    modifies most of Home.js to fit url shortener homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Row, Col, Form, FormGroup, Input, Label, Popover, PopoverBody } from 'reactstrap';
-import './App.css';
 import product from './product.jpg';
 import icon from './icon.png';
 import book from './book.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import {Footer} from './Footer';                                                
+import {Footer} from './Footer';
+import './App.css';
 
 
 export class Home extends Component {
@@ -22,8 +22,9 @@ export class Home extends Component {
 			data: [],
 			id: 0,
 			urlToShorten: null,
-			shortenedUrl: null,
+			shortenedUrl: 'https://mernurl.herokuapp.com/',
 			popoverOpen: false,
+			linksPowered: 0,
 			intervalIsSet: false,
 			idToDelete: null,
 			idToUpdate: null,
@@ -34,8 +35,8 @@ export class Home extends Component {
 		this.callback = this.callback.bind(this);
 		this.handleClick = this.handleClick.bind(this);
 		this.toggle = this.toggle.bind(this);
-		/*this.getDataFromDb = this.getDataFromDb.bind(this);
-		this.deleteFromDB = this.deleteFromDB.bind(this);
+		this.getDataFromDb = this.getDataFromDb.bind(this);
+		/*this.deleteFromDB = this.deleteFromDB.bind(this);
 		this.updateDB = this.updateDB.bind(this);*/
 	}
 
@@ -44,13 +45,11 @@ export class Home extends Component {
 	// changed and implement those changes into our UI
 	componentDidMount() {
 		window.scrollTo(0,0);
-		/*
 		this.getDataFromDb();
 		if (!this.state.intervalIsSet) {
 			let interval = setInterval(this.getDataFromDb, 1000);
 			this.setState({ intervalIsSet: interval });
 		}
-		*/
 	}
 
 	// never let a process live forever 
@@ -61,7 +60,7 @@ export class Home extends Component {
 			this.setState({ intervalIsSet: null });
 		}
 	}
-
+*/
 	// our first get method that uses our backend api to 
 	// fetch data from our data base
 	getDataFromDb = () => {
@@ -70,6 +69,7 @@ export class Home extends Component {
 			.then(res => this.setState({ data: res.data }))
 			.then(console.log(this.state.data));
 	};
+	/*
 
 */
 	//consol.log's the new document made by putDataToDB
@@ -211,156 +211,54 @@ export class Home extends Component {
 
 			
 			<div className='description'>
-				<h2>SECTION TITLE</h2>
-				<p>Nunc vulputate lobortis lectus, rutrum porttitor justo consequat at quisque mi odio, consectetur commodo.</p>
-			</div>
-
-			<div className='capstone-feature'>
-				<Container><Row>
-					<Col lg='4'>
-						<h2>CAPSTONE TITLE</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam nunc aliquam magna suscipit, eu pharetra metus maximus. Nunc ac est posuere, pellentesque odio at, hendrerit justo. Donec in blandit urna. </p> 
-						<p>Sed a nunc ut ex dignissim dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rutrum suscipit sagittis. Proin in tellus lectus. Vivamus quis erat diam. Donec et ligula commodo, pretium ligula a, ornare dolor.</p>
-					</Col>
-					<Col lg='8' className='product-col'>
-						<img className='product' src={product}/>
-					</Col>
-				</Row></Container>
-			</div>
-
-			<div className='features-header'>
-				<h2>FEATURES</h2>
-				<p>Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis.</p>
-			</div>
-
-			<div className='features'>
-				<Container>
+				<h2>UNLEASH THE POWER OF THE LINK</h2>
+				<p className='desc-p'>Hyperlinks are everywhere and they can lead you to just as many places. Help others by saving space on the interweb and shortening your links using the form above.</p>
+				<Container className='perk-cards-container'>
 					<Row>
-						<Row>
-						<Col xs='2' md='1'>
-							<div className='feature-icon'><img src={icon}/></div>
+						<Col md='4' className='perk-card'>
+							<FontAwesomeIcon icon={faCoffee} size='3x' transform='grow-1' color='#007bff'/>
+							<h3 className='card-h3'>Perk One</h3>
+							<p className='card-p'>Pellentesque eleifend arcu nisi, feugiat efficitur ligula pharetra non. Morbi nunc neque, lacinia quis nisl a, volutpat volutpat enim. Morbi est lectus, luctus sit amet metus ac, porttitor tristique dui. Phasellus rhoncus odio non erat rutrum efficitur porta nec ipsum. </p>
 						</Col>
-						<Col xs='10' md='5'>
-							<div className='feature-desc'>
-								<h3>Feature Name</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis. Pellentesque volutpat urna vitae justo dictum imperdiet. </p>
-							</div>
+						<Col md='4' className='perk-card'>
+							<FontAwesomeIcon icon={faCoffee} size='3x' transform='grow-1' color='#007bff'/>
+							<h3 className='card-h3'>Perk Two</h3>
+							<p className='card-p'>Pellentesque eleifend arcu nisi, feugiat efficitur ligula pharetra non. Morbi nunc neque, lacinia quis nisl a, volutpat volutpat enim. Morbi est lectus, luctus sit amet metus ac, porttitor tristique dui. Phasellus rhoncus odio non erat rutrum efficitur porta nec ipsum. </p>
 						</Col>
-
-						
-						<Col xs='2' md='1'>
-							<div className='feature-icon'><img src={icon}/></div>
-						</Col>
-						<Col xs='10' md='5'>
-							<div className='feature-desc'>
-								<h3>Feature Name</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis. Pellentesque volutpat urna vitae justo dictum imperdiet. </p>
-							</div>
-						</Col>
-						</Row>
-					</Row>
-					
-					<Row>
-						<Row>
-						<Col xs='2' md='1'>
-							<div className='feature-icon'><img src={icon}/></div>
-						</Col>
-						<Col xs='10' md='5'>
-							<div className='feature-desc'>
-								<h3>Feature Name</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis. Pellentesque volutpat urna vitae justo dictum imperdiet. </p>
-							</div>
-						</Col>
-
-						
-						<Col xs='2' md='1'>
-							<div className='feature-icon'><img src={icon}/></div>
-						</Col>
-						<Col xs='10' md='5'>
-							<div className='feature-desc'>
-								<h3>Feature Name</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis. Pellentesque volutpat urna vitae justo dictum imperdiet. </p>
-							</div>
-						</Col>
-						</Row>
-					</Row>
-
-					<Row>
-						<Row>
-						<Col xs='2' md='1'>
-							<div className='feature-icon'><img src={icon}/></div>
-						</Col>
-						<Col xs='10' md='5'>
-							<div className='feature-desc'>
-								<h3>Feature Name</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis. Pellentesque volutpat urna vitae justo dictum imperdiet. </p>
-							</div>
-						</Col>
-
-						
-						<Col xs='2' md='1'>
-							<div className='feature-icon'><img src={icon}/></div>
-						</Col>
-						<Col xs='10' md='5'>
-							<div className='feature-desc'>
-								<h3>Feature Name</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis. Pellentesque volutpat urna vitae justo dictum imperdiet. </p>
-							</div>
-						</Col>
-						</Row>
+						<Col md='4' className='perk-card'>
+							<FontAwesomeIcon icon={faCoffee} size='3x' transform='grow-1' color='#007bff'/>
+							<h3 className='card-h3'>Perk Three</h3>
+							<p className='card-p'>Pellentesque eleifend arcu nisi, feugiat efficitur ligula pharetra non. Morbi nunc neque, lacinia quis nisl a, volutpat volutpat enim. Morbi est lectus, luctus sit amet metus ac, porttitor tristique dui. Phasellus rhoncus odio non erat rutrum efficitur porta nec ipsum. </p>
+						</Col>				
 					</Row>
 				</Container>
 			</div>
 
-			<div className='additional-product'>
-				<h2>ADDITIONAL RELEVANT PRODUCT</h2>
-				<p>Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis.</p>
+
+
+			<div className='frame-frame'>
+				<div className='frame-div'>
+					<iframe 
+						src="https://www.youtube.com/embed/lSX5dPdArZ0" 
+						frameborder="0" 
+						allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" 
+						allowfullscreen/>
+				</div>
+
+
 				<div className='btn-holder'>
 					<Button color='primary'>LEARN MORE</Button>
 				</div>
 			</div>
 
-			<div className='defining-value'>
-				<h2>IMPORTANT VALUE</h2>
-				<p>Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis.</p>
-			</div>
 
-			<div className='product-giveaway'>
-				<Container><Row>
-					<Col sm='7'>
-						<h2>FREE PRODUCT</h2>
-						<p>Fusce ut leo purus. Morbi consequat dui at ligula blandit, quis aliquet magna consectetur. Quisque mollis id tortor a iaculis.</p>
-						<div className='btn-holder'>
-							<Button color='primary'>DOWNLOAD</Button>
-						</div>
-					</Col>
-					<Col sm='5' className='product-col'>
-						<img src={book}/>
-					</Col>
-				</Row></Container>
-			</div>
 
 			<div className='ending'>
 				<Container><Row>
 					<Col lg='4'>
 						<div className='quantifiable'>
-							<FontAwesomeIcon icon={faCoffee} size='2x' transform='grow-7'/>
-							<h2>39%</h2>
-							<h3>Increase in sales</h3>
-						</div>
-					</Col>
-					<Col lg='4'>
-						<div className='quantifiable'>
-							<FontAwesomeIcon icon={faCoffee} size='2x' transform='grow-7'/>
-							<h2>99%</h2>
-							<h3>Customer Satisfaction</h3>
-						</div>
-					</Col>
-					<Col lg='4'>
-						<div className='quantifiable'>
-							<FontAwesomeIcon icon={faCoffee} size='2x' transform='grow-7'/>
-							<h2>385000</h2>
-							<h3>Bookings</h3>
+							<h2>{this.state.linksPowered}</h2>
+							<h3 className='ending-h3'>LINKS POWERED BY MERNURL</h3>
 						</div>
 					</Col>
 				</Row></Container>
